c9638151b8f1f2cd5aae76982044b922b4856121
charles <charles@82e7c6bb-cafa-0310-8398-98fdc49d3415>
2007-05-03 00:06:44 +0000

    Xgrid FUSE: added GPL license text in all source files.
    git-svn-id: file:///Users/charles/Docs/Dev+/Repository/XgridFUSE/trunk@253 82e7c6bb-cafa-0310-8398-98fdc49d3415

--- NSInvocationXgridFuseCategory.h
+++ NSInvocationXgridFuseCategory.h
@@ -3,10 +3,17 @@
 //  Xgrid FUSE
 //
 //  Created by Charles Parnot on 3/20/07.
-//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//  Copyright 2007 Charles Parnot. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+/*
+ This file is part of Xgrid FUSE.
+ Xgrid FUSE is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ Xgrid FUSE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License along with Xgrid FUSE; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/* NOT USED */
 
 /* this was initially used to communicate from the fuse thread to the main thread in a supposedly clean way; but then, the application was locking on some calls, apparently, so I gave up for now */
 
